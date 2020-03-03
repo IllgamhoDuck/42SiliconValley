4562e6564670c283773697a0c6adaef27370d901
Hyunbyung Park <hyunbyung87@gmail.com>
2020-03-02 21:52:09 -0800

    ex04 getplace

---
+++ interview_piscine/d04/ex04/getPlace.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   getPlace.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hypark <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/02 20:55:05 by hypark            #+#    #+#             */
+/*   Updated: 2020/03/02 21:51:09 by hypark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h> //printf, scanf, ...
+#include <string.h> //memcpy, strlen, ...
+#include <unistd.h> //fork, write, sleep...
+#include <stdlib.h> //malloc, free, exit...
+
+#include "header.h"
+
+int getPlace(unsigned int parkingRow, int pos)
+{
+	return (parkingRow >> pos) & 1;
+}
